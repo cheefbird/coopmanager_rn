61f93213370f3f01497530b805100af8a3bb0420
Francis Breidenbach <fbreidenbach@gmail.com>
2018-10-20 01:48:32 -0700

    Create theme fonts and colors.

---
+++ src/Theme/Colors.js
@@ -0,0 +1,11 @@
+// @flow
+
+const Colors = {
+  darkGreen: '#2e382e',
+  tealBlue: '#50c9ce',
+  midBlue: '#72a1e5',
+  purple: '#9883e5',
+  pink: '#fcd3de',
+};
+
+export default Colors;
---
+++ src/Theme/index.js
@@ -0,0 +1,6 @@
+// @flow
+
+import Colors from './Colors';
+import Fonts from './Fonts';
+
+export { Colors, Fonts };
